1066c9b31918608405305bda3e5dfa52472de7c8
mpedrano <mpedrano@my.bcit.ca>
2024-03-11 17:19:41 -0700

    add navbar

---
+++ pages/products.js
@@ -0,0 +1,10 @@
+import styles from "@/styles/Products.module.css";
+import NavBar from "@/components/NavBar";
+
+export default function Products() {
+    return (
+        <>
+            <NavBar />
+        </>
+    )
+}
