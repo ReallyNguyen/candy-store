423f31db609376342d8a85e150d68ff35d351c4e
mpedrano <mpedrano@my.bcit.ca>
2024-03-11 18:43:22 -0700

    update products page

--- components/ProductCard/index.js
+++ components/ProductCard/index.js
@@ -1,6 +1,13 @@
+import styles from './ProductCard.module.css'
+import Image from 'next/image'
+
 export default function ProductCard() {
     return (
-        <>
-        </>
+        <div className={styles.container}>
+            <Image src={"/logo.svg"} alt="logo" width={100} height={100} />
+            <h2>Candy</h2>
+            <h3>$0.00</h3>
+            <button className={styles.btn}>Add to Cart</button>
+        </div>
     )
 }
--- pages/products.js
+++ pages/products.js
@@ -1,10 +1,34 @@
 import styles from "@/styles/Products.module.css";
 import NavBar from "@/components/NavBar";
+import ProductCard from "@/components/ProductCard";
 
 export default function Products() {
     return (
         <>
             <NavBar />
+            <div className={styles.container}>
+                <h1>Shop All Candy</h1>
+                <div className={styles.selects}>
+                    <select name="filter" className={styles.filter}>
+                        <option value="">Filter</option>
+                        <optgroup label="Price">
+                            <option value="low">$0-5</option>
+                            <option value="high">$6-10</option>
+                        </optgroup>
+                        <optgroup label="Availability">
+                            <option value="stock">In Stock</option>
+                        </optgroup>
+                    </select>
+                    <select name="sort" className={styles.sort}>
+                        <option value="">Sort</option>
+                        <option value="best">Best Selling</option>
+                        <option value="alphabetical">Alphabetical, A-Z</option>
+                        <option value="low">Price, low to high</option>
+                        <option value="high">Price, high to low</option>
+                    </select>
+                </div>
+                <ProductCard />
+            </div>
         </>
     )
 }
